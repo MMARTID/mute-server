4bd2dc0a040e0362f044489c4bc0151f11d21723
MMARTID <miguelmartindelgado1998@gmail.com>
2025-01-22 10:28:58 +0100

    dep config

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 // conectarnos a la DB
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mute";
 mongoose
-.connect('mongodb://localhost:27017/mute')
-.then(response => console.log(`Connected to ${response.connections[0].name} database`))
-.catch(error => console.log("Error connecting to MongoDB", error));
+  .connect(MONGO_URI)
+  .then((response) =>
+    console.log(`Connected to ${response.connections[0].name} database`)
+  )
+  .catch((error) => console.log("Error connecting to MongoDB", error));
